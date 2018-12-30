d5c157b6d344d8515c9843b047a24d884e2e8ef3
Anton Björkman <antonidag@hotmail.com>
2018-12-30 02:04:10 +0100

    Big Fix
    Fixed so u can click on fire to chage that background.

--- blend/script.js
+++ blend/script.js
@@ -1,31 +1,43 @@
-var itemFire = document.getElementById('itemFire');
-var itemMountain = document.getElementById('itemMountain');
-var itemAirballon = document.getElementById('itemAirballon');
+
+var target = "";
 var bg = document.getElementById("bg");
 
 
+$( ".item" ).click(function() {
+  var rawStr = $(this).text();
+  var str = rawStr.trim();
+  console.log(str);
 
+  switch (str) {
+    case "Fire":
+      $("#menu").fadeOut();
+      target = "image/fire/fire";
+      globalTicker(target);
+      break;
+    case "Sky":
+      $("#menu").fadeOut();
+      break;
+    case "Nature":
+      $("#menu").fadeOut();
+      break;
+    case "Ocean":
+      $("#menu").fadeOut();
+      break;
+    default:
 
-itemFire.addEventListener('click', function() {
-  fire();
+  }
+  $("#bg").fadeIn();
 });
 
-itemMountain.addEventListener('click', function(){
-  bg.style.backgroundImage = "url('image/mountain.jpg')";
-});
 
-itemAirballon.addEventListener('click', function(){
-  bg.style.backgroundImage = "url('image/airballon.jpg')";
-});
+
+    //bg.style.backgroundImage = "url('image/fire/fireplace" +  current % 2 + ".jpg'";
 
 var current = -1;
-function fire() {
-  current ++;
-  bg.style.backgroundImage = "url('image/fire/fireplace" +  current % 2 + ".jpg'";
-  console.log(current % 2);
-  setTimeout(() => fire(), 15000);
-
-}
-function globaltick(){
-
+function globalTicker(target) {
+  if(target != ""){
+    current ++;
+    bg.style.backgroundImage = "url('"+ target + current % 2 + ".jpg')";
+    setTimeout(() => globalTicker(target), 15000);
+  }
 }
