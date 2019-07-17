76f1df1c8c1132d5a93f8f08d2c363e21f306b8c
Anton Björkman <antonidag@hotmail.com>
2019-07-17 22:10:44 +0200

    Add new feature changing element lead image.

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+
+
+//var url ='http://www.splashbase.co/api/v1/images/search?query=';
+/*$.getJSON('https://geoip-db.com/json/')
+   .done (function(location) {
+    buildUlr = url.concat(location.city);
+  }).then( () => {
+
+  });*/
+//
+var url = 'http://www.splashbase.co/api/v1/images/latest'
+  $.ajax({
+      url: url,
+      type: 'GET',
+      success: function(data) {
+        console.log(url);
+         console.log(data);
+       }
+  }).then( (data) => {
+
+    while(true){
+      var rndNum =Math.floor(Math.random() * data.images.length);
+      var stringImage = data.images[rndNum].large_url;
+      if(stringImage.match(/\.(gif|jpg|jpeg|tiff|png)$/i)){
+        break;
+      }
+    }
+    $("#lead").css("background-image","url(" + stringImage + ")");
+  });
